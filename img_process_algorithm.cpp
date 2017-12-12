94687bf4e81e9101dd595146d7557210342cb3c0
phuongzzz <yesterdayoncemore23@gmail.com>
2017-12-12 16:49:15 +0700

    done histogram eq gray

--- img_process_algorithm.cpp
+++ img_process_algorithm.cpp
@@ -7,9 +7,26 @@
 using namespace std;
 using namespace cv;
 
+/*Tang cuong do tuong phan*/
+void histogrameq_for_grayImg(Mat image);
+void histogrameq_for_colorImg(Mat image);
+/*nhan chap*/
 void nhanchap(Mat image, int kernel_size);
-void timbien();
-void histogrameq();
+/*Lam tron anh bang loc thong thap*/
+void gaussian(Mat image);
+void medianFilter(Mat image);
+/*Phat hien canh bang loc thong cao*/
+void laplace(Mat image);
+void sobel(Mat image);
+void canny(Mat image);
+/*xu ly anh trong mien tan so*/
+void dft(Mat image);
+/*phan vung anh*/
+void kmeans(Mat image);
+void watershared(Mat image);
+/*hau xu ly anh*/
+void morphology(Mat image);
+/*thoat chuong trinh*/
 void exit();
 
 int selection = 0;
@@ -23,19 +40,39 @@ int main(int argc, char** argv) {
   while (k == 0) {
     selection = 0;
     cout << " Please choose from the following options: \n";
-    cout << " 1. Nhan Chap.\n";
-    cout << " 2. Set launch codes.\n";
-    cout << " 3. Exit.\n";
+    cout << " 1. Increasing Constrast.\n";
+    cout << " 2. Convolution.\n";
+    cout << " 3. Smoothing image.\n";
+    cout << " 4. Edge detection.\n";
+    cout << " 5. Discrete Fourier Transform.\n";
+    cout << " 6. Image Segmentation.\n";
+    cout << " 7. Morphology.\n";
+    // cout << " 8.Canny\n";
+    // cout << " 9.DFT.\n";
+    // cout << " 10.Kmeans.\n";
+    // cout << " 11.Watershared.\n";
+    // cout << " 12.Morphology.\n";
+    cout << " 8. Exit.\n";
     cout << "Your selection:\t";
     cin >> selection;
 
   switch (selection) {
     case 1: {
-      nhanchap(image, 6);
+      cout << "Chon phuong phap can lam" << endl;
+      cout << "a. Histogram Equalize (for grayscale image)" << endl;
+      cout << "b. Histogram Equalize (for color image)" << endl;
+      char select1;
+      cin >> select1;
+      switch(select1) {
+        case 'a': {
+          histogrameq_for_grayImg(image);
+          break;
+        }
+      }
       break;
     }
     case 2: {
-      canny();
+      // canny();
       break;
     }
     case 3: {
@@ -64,8 +101,58 @@ void nhanchap(Mat image, int kernel_size) {
   waitKey(0);
 }
 
-void canny() {
-  cout << "canny" << endl;
+void histogrameq_for_grayImg(Mat image){
+  Mat image_dst;
+  char* source_window = "Source image";
+  char* equalized_window = "Equalized Image";
+
+  cvtColor( image, image, CV_BGR2GRAY );
+
+  equalizeHist( image, image_dst );
+
+  namedWindow( source_window, CV_WINDOW_AUTOSIZE );
+  namedWindow( equalized_window, CV_WINDOW_AUTOSIZE );
+
+  imshow( source_window, image );
+  imshow( equalized_window, image_dst );
+
+  waitKey(0);
+}
+
+void histogrameq_for_colorImg(Mat image){
+  //todo
+}
+
+void gaussian(Mat image){
+  //todo
+}
+
+void medianFilter(Mat image){
+  //todo
+}
+void laplace(Mat image){
+  //todo
+}
+void sobel(Mat image){
+  //todo
+}
+void canny(Mat image) {
+  //todo
+}
+void dft(Mat image) {
+  //todo
+}
+
+void kmeans(Mat image) {
+  //todo
+}
+
+void watershared(Mat image) {
+  //todo
+}
+
+void morphology(Mat image) {
+  //todo
 }
 
 void exit() {
